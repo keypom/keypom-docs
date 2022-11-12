084ea8ed1fc9b6fe848039add9d53c8526e647fb
Min Qian Lu <min@v1040-wn-rt-a-120-55.campus-dynamic.uwaterloo.ca>
2022-11-12 17:31:49 -0500

    removing reed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         items: [
           {
             type: 'doc',
-            docId: 'docs/Github ReadMe/Introduction/Introduction',
+            docId: 'Github ReadMe/Introduction/Introduction',
             position: 'left',
             label: 'Tutorial',
           },
